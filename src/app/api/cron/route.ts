fc48ce4f8eed05e589ec45126b23392527cff96f
rasyidcode <rasyidcode7@gmail.com>
2024-01-31 14:51:11 +0700

    debug vercel

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,47 +2,59 @@ import { getChapterImageURLs } from "@/app/utils";
 import { PutBlobResult, list, put } from "@vercel/blob";
 
 export async function GET() {
-    const imageurls = await getChapterImageURLs();
-    if (imageurls == null || imageurls.length == 0) {
-        return Response.json({
-            success: false,
-            message: 'Failed to fetch images'
-        });
-    }
+    try {
+        const imageurls = await getChapterImageURLs();
 
-    // check if blob already exist
-    const storedblobs = await list({ token: process.env.BLOB_OP_LAST_CHP_READ_WRITE_TOKEN })
-    if (storedblobs.blobs.length > 0) {
-        const firstblobpathname = storedblobs.blobs.at(0)?.pathname as string;
-        for(const imageurl of imageurls) {
-            if (imageurl.includes(firstblobpathname)) {
-                return Response.json({
-                    success: true,
-                    message: 'Images already stored.',
-                    data: storedblobs
-                })
+        // check if blob already exist
+        const storedblobs = await list({ token: process.env.BLOB_OP_LAST_CHP_READ_WRITE_TOKEN })
+        if (storedblobs.blobs.length > 0) {
+            const firstblobpathname = storedblobs.blobs.at(0)?.pathname as string;
+            for (const imageurl of imageurls) {
+                if (imageurl.includes(firstblobpathname)) {
+                    return Response.json({
+                        success: true,
+                        message: 'Images already stored.',
+                        data: storedblobs
+                    })
+                }
             }
         }
+
+        const blobs: PutBlobResult[] = [];
+
+        // save to vercel blob
+        for (const imageurl of imageurls) {
+            const match = imageurl.match(/(?:\/)([^\/]+)$/);
+            if (match) {
+                const filename = match[1];
+
+                const response = await fetch(imageurl);
+                const blob = await put(filename, await response.blob(), { access: 'public', token: process.env.BLOB_OP_LAST_CHP_READ_WRITE_TOKEN });
+
+                blobs.push(blob);
+            };
+        }
+
+        return Response.json({
+            success: true,
+            message: 'Images stored',
+            data: blobs
+        });
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            return Response.json({
+                success: false,
+                error: {
+                    message: e.message,
+                    name: e.name,
+                    stack: e.stack
+                },
+            });
+        }
+
+        return Response.json({
+            success: false,
+            error: 'Unkown error'
+        });
     }
-
-    const blobs: PutBlobResult[] = [];
-
-    // save to vercel blob
-    for (const imageurl of imageurls) {
-        const match = imageurl.match(/(?:\/)([^\/]+)$/);
-        if (match) {
-            const filename = match[1];
-
-            const response = await fetch(imageurl);
-            const blob = await put(filename, await response.blob(), { access: 'public', token: process.env.BLOB_OP_LAST_CHP_READ_WRITE_TOKEN });
-
-            blobs.push(blob);
-        };
-    }
-
-    return Response.json({
-        success: true,
-        message: 'Images stored',
-        data: blobs
-    });
 }
--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,26 +1,22 @@
 import puppeteer from "puppeteer";
 
-export async function getChapterImageURLs(): Promise<string[]|null> {
-    try {
-        const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
-        const page = await browser.newPage();
+export async function getChapterImageURLs(): Promise<string[]> {
+    const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
+    const page = await browser.newPage();
 
-        page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
-        await page.goto('https://tcbscans.com');
+    await page.goto('https://tcbscans.com');
 
-        await page.waitForSelector('text/One Piece');
-        await page.click('text/One Piece');
+    await page.waitForSelector('text/One Piece');
+    await page.click('text/One Piece');
 
-        await page.waitForSelector('picture.fixed-ratio img.fixed-ratio-content');
-        const images = await page.$$eval('picture.fixed-ratio > img.fixed-ratio-content', im => im.map(m => m.getAttribute('src') as string));
+    await page.waitForSelector('picture.fixed-ratio img.fixed-ratio-content');
+    const images = await page.$$eval('picture.fixed-ratio > img.fixed-ratio-content', im => im.map(m => m.getAttribute('src') as string));
 
-        await page.evaluate(() => console.log('url is ' + location.href));
+    await page.evaluate(() => console.log('url is ' + location.href));
 
-        await browser.close();
+    await browser.close();
 
-        return images;
-    } catch(e: unknown) {
-        return null;
-    }
+    return images;
 }
