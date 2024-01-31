9bc7bcaa9a52702276e909175058b465d122fd95
rasyidcode <rasyidcode7@gmail.com>
2024-01-31 17:07:17 +0700

    use chromium min instead

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,19 +1,21 @@
-import Chromium from "@sparticuz/chromium";
+import Chromium from "@sparticuz/chromium-min";
 import puppeteer, { Browser } from "puppeteer-core";
 
 export async function getChapterImageURLs(): Promise<string[]> {
     let browser: Browser = await puppeteer.launch({
         args: Chromium.args,
         defaultViewport: Chromium.defaultViewport,
-        executablePath: await Chromium.executablePath(),
+        executablePath: await Chromium.executablePath('https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar'),
         headless: Chromium.headless,
+        ignoreHTTPSErrors: true,
     });
 
     const page = await browser.newPage();
 
     page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    // page.once('load', () => console.log('Page loaded'));
 
-    await page.goto('https://tcbscans.com');
+    await page.goto('https://tcbscans.com', { waitUntil: 'networkidle0'});
 
     await page.waitForSelector('text/One Piece');
     await page.click('text/One Piece');
@@ -23,7 +25,8 @@ export async function getChapterImageURLs(): Promise<string[]> {
 
     await page.evaluate(() => console.log('url is ' + location.href));
 
+    await page.close();
     await browser.close();
 
-    return images;
+    return images; 
 }
